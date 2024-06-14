dcaf0882b2bf07c1368e2817362ee905fbd6b7f4
DryingCore <lein0rb4g@gmail.com>
2024-06-14 12:30:23 +0000

    Jun 14, 2024, 9:30 AM

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+const elementosLeft = document.querySelectorAll("#left");
 
-const elementosLeft = document.querySelectorAll('.left')
-
-  elementosLeft.forEach(function (left) { left.addEventListener('click', function () {
-  left.classList.toggle('ativa')
-  })
-})
+elementosLeft.forEach(function (left) {
+	left.addEventListener("click", function () {
+		left.classList.toggle("ativa");
+	});
+});
